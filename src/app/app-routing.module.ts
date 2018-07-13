7a1808acf4b45edc2caeae4c9fdbdc542bdf075b
Kevin Hinojosa <kevi123321@gmail.com>
2018-07-12 18:56:49 -0700

    angukar first push

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import{ NgModule } from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {HomeComponent} from './home/home.component'
+import {ContactComponent} from './contact/contact.component'
+import {TodoComponent} from './todo/todo.component'
+//./home/home.component/  .<---means current ditectory
+
+const routes: Routes = [
+  {path: '', component: HomeComponent },
+  {path: 'contact', component:ContactComponent},
+  {path: 'todo', component: TodoComponent}
+
+];
+@NgModule({
+  imports:
+  [RouterModule.forRoot(routes)
+],
+  exports:[RouterModule ]
+})
+export class AppRoutingModule{}
